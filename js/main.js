db1b65f08e4ac0e9f93366d6eb873dc9a45eca44
jerp86 <jerp4@hotmail.com>
2020-12-07 19:39:59 -0300

    Exibindo uma tabela com o resultado

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ function consultaCep() {
     type: 'GET',
     success: function(response) {
       console.log(response);
+      $('#logradouro').html(`${response.logradouro || "Não existe"} - ${response.complemento || ''}`);
+      $('#bairro').html(response.bairro || "");
+      $('#localidade').html(response.localidade || "");
+      $('#uf').html(response.uf || "");
+      $('#titulo_cep').html(`CEP: ${response.cep || "Número digitado não existe!"}`);
+      $(".container").css("display", "block");
     },
+    error: function() {
+      $(".container").css("display", "none");
+    }
   });
 }
