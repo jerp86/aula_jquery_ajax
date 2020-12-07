b4d6ff4720343e932671b161a60b703415c0b835
jerp86 <jerp4@hotmail.com>
2020-12-07 19:03:12 -0300

    Estilizando a página

--- js/main.js
+++ js/main.js
@@ -6,11 +6,6 @@ function consultaCep() {
     type: 'GET',
     success: function(response) {
       console.log(response);
-      // document.getElementById('logradouro').innerHTML = response.logradouro;
-      $('#logradouro').html(`${response.logradouro} - ${response.complemento}`);
-      document.getElementById('bairro').innerHTML = response.bairro;
-      document.getElementById('localidade').innerHTML = response.localidade;
-      document.getElementById('uf').innerHTML = response.uf;
     },
   });
 }
