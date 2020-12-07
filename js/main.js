f50b145aaed8a3fcb3d6875f97ecf7588679d83b
jerp86 <jerp4@hotmail.com>
2020-12-07 18:18:36 -0300

    Mudando o texto para template string

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function consultaCep() {
     success: function(response) {
       console.log(response);
       // document.getElementById('logradouro').innerHTML = response.logradouro;
-      $('#logradouro').html(response.logradouro+' - '+response.complemento);
+      $('#logradouro').html(`${response.logradouro} - ${response.complemento}`);
       document.getElementById('bairro').innerHTML = response.bairro;
       document.getElementById('localidade').innerHTML = response.localidade;
       document.getElementById('uf').innerHTML = response.uf;
