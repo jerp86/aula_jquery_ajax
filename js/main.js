e6912d2b2fcbb023128497fafffc99765c207cf8
jerp86 <jerp4@hotmail.com>
2020-12-07 17:59:48 -0300

    Iniciando com jQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert("Hello World!");
+function consultaCep() {
+  const cep = document.getElementById("cep").value;
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  $.ajax({
+    url,
+    type: 'GET',
+    success: function(response) {
+      console.log(response);
+      // document.getElementById('logradouro').innerHTML = response.logradouro;
+      $('#logradouro').html(response.logradouro+' - '+response.complemento);
+      document.getElementById('bairro').innerHTML = response.bairro;
+      document.getElementById('localidade').innerHTML = response.localidade;
+      document.getElementById('uf').innerHTML = response.uf;
+    },
+  });
+}
